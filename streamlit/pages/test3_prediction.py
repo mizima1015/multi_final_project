0ece8f1e4c1c062ecee5a961c891d3d843eb7262
Mizima <mizimaaz@gmail.com>
2023-11-15 11:42:14 +0900

    test

--- streamlit/pages/test3_prediction.py
+++ streamlit/pages/test3_prediction.py
@@ -6,8 +6,11 @@ import joblib
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 
-plt.rcParams['font.family'] = 'Malgun Gothic'
+
+plt.rcParams['font.family'] = 'NanumBarunGothic'
 plt.rcParams['axes.unicode_minus'] = False
+# plt.rcParams['font.family'] = 'Malgun Gothic'
+# plt.rcParams['axes.unicode_minus'] = False
 
 # 모델을 불러오는 함수
 def load_model(model_name):
