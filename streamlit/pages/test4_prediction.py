04e32b8373a1a8d1fba1f4d0cfc9d68ffbb374a5
Mizima <mizimaaz@gmail.com>
2023-11-21 00:35:09 +0900

    error

--- streamlit/pages/test4_prediction.py
+++ streamlit/pages/test4_prediction.py
@@ -9,10 +9,12 @@ import numpy as np
 
 # 사용자 정의 폰트 경로
 font_path = './customFonts/NanumGothic-Regular.ttf'
+
 # 폰트 매니저에 폰트를 등록
 font_prop = fm.FontProperties(fname=font_path)
 fm.fontManager.addfont(font_path)
 rc('font', family=font_prop.get_name())
+
 # 한글 마이너스 기호 문제 해결을 위한 설정
 rc('axes', unicode_minus=False)
 
@@ -33,43 +35,20 @@ def plot_bar_chart(predictions, labels):
     # 색상 배열을 생성합니다. 이 배열은 예측값의 개수만큼의 색상을 viridis 색상맵에서 균등하게 선택합니다.
     colors = plt.cm.viridis(np.linspace(0, 1, len(predictions)))
     
-    if len(predictions) == 1:
-        bar_width = 0.2
-    elif len(predictions) == 2:
-        bar_width = 0.4
-    else :
-        bar_width =0.6
-
     plt.figure(figsize=(10, 7))
-    bars = []  # 막대 객체를 담을 리스트
-
-    # 각 예측에 대해 개별적으로 막대를 그리고 레이블을 지정합니다.
-    for i, (label, prediction) in enumerate(zip(labels, predictions)):
-        bar = plt.bar(label, prediction, color=colors[i], width=bar_width, label=label)
-        bars.append(bar)
-
+    bars = plt.bar(labels, predictions, color=colors)
+    
     # 막대 그래프 위에 값을 표시합니다.
     for bar in bars:
-        for item in bar:
-            yval = item.get_height()
-            plt.text(item.get_x() + item.get_width() / 2, yval, round(yval, 1), va='bottom', ha='center')
+        yval = bar.get_height()
+        plt.text(bar.get_x() + bar.get_width() / 2, yval, round(yval, 2), va='bottom', ha='center')
     
     plt.xlabel('품목')
     plt.ylabel('물류량')
-    plt.xticks(rotation=90)
-
-    # x축의 범위를 설정하여 뚱뚱한 막대가 나오지 않도록 조정합니다.
-    plt.xlim(-0.5, len(labels)-0.5)
-    
+    plt.xticks(rotation=45)
     plt.tight_layout()
-    
-    # 범례를 추가합니다. 'best' 위치에 표시되도록 설정합니다.
-    plt.legend(loc='lower center', bbox_to_anchor=(0.5, 1), ncol=3)
-
-
-
     st.pyplot(plt)
-# 예측 페이지를 보여주는 함수
+
 # 예측 페이지를 보여주는 함수
 def show_prediction_page(all_terminals):
     st.set_page_config(layout="wide")
@@ -147,10 +126,5 @@ def show_prediction_page(all_terminals):
         # 예측 결과 출력
         st.write(f'선택된 날짜 범위에 대한 예측된 물류량 합산: {sum(predictions):.2f}')
 
-    if st.button('그래프 초기화'):
-        st.session_state.predictions.clear()
-        st.session_state.labels.clear()
-        st.experimental_rerun()
-        
 if __name__ == "__main__":
-    show_prediction_page(['강남', '강동', '강북', ...])
+    show_prediction_page(['강남', '강동', '강북', ...])  # 터미널 이름 리스트를 채워주세요.
